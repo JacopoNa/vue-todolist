b9b55269d67116fb7802f4517df2975ed9379736
JacopoNa <j.nardelli@virgilio.it>
2022-06-27 15:23:20 +0200

    milestone-1

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante,
 // il testo digitato viene letto e utilizzato per creare un nuovo todo,
 // che quindi viene aggiunto alla lista dei todo esistenti.
+
+// ---------------------------------------------------------------
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            toDoList: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    }
+)
